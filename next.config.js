bfaa8752d5383a5ff3861bcb9d5497542d6c4265
dossahroderic-droid <dossahroderic@icloud.com>
2026-04-05 05:46:46 +0000

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,7 +1,13 @@
 /** @type {import('next').NextConfig} */
+const { i18n } = require('./next-i18next.config');
+
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  i18n: {
+    locales: ['fr', 'wo', 'en', 'ar'],
+    defaultLocale: 'fr',
+  },
   images: {
     domains: ['res.cloudinary.com'],
   },
