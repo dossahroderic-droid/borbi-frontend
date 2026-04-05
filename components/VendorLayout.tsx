74018ed33d3115aa0bf440ad702586be36a0f010
dossahroderic-droid <dossahroderic@icloud.com>
2026-04-05 04:43:06 +0000

    Update VendorLayout.tsx

--- components/VendorLayout.tsx
+++ components/VendorLayout.tsx
@@ -1,11 +1,15 @@
 import VendorSidebar from './VendorSidebar';
+import OnlineStatus from './OnlineStatus';
 
 export default function VendorLayout({ children }: { children: React.ReactNode }) {
   return (
     <div className="flex min-h-screen bg-gray-100">
       <VendorSidebar />
-      <div className="flex-1 ml-64 p-6">
-        <div className="bg-white rounded-lg shadow p-6">{children}</div>
+      <div className="flex-1 ml-64">
+        <div className="bg-white border-b p-4 flex justify-end items-center gap-4">
+          <OnlineStatus />
+        </div>
+        <div className="p-6">{children}</div>
       </div>
     </div>
   );
